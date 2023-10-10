e796616f8b973ab81f5c7fd0fa41293e26a05d06
vanluong2003 <adhotran2003@gmail.com>
2023-10-11 01:38:58 +0700

    codelab4

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+        rollDice()
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // Update the ImageView with the correct drawable
+        when(diceRoll){
+            1->diceImage.setImageResource(R.drawable.dice_1)
+            2->diceImage.setImageResource(R.drawable.dice_2)
+            3->diceImage.setImageResource(R.drawable.dice_3)
+            4->diceImage.setImageResource(R.drawable.dice_4)
+            5->diceImage.setImageResource(R.drawable.dice_5)
+            6->diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+        // Update the content descriptio
+        diceImage.contentDescription = diceRoll.toString()
+    }
+}
+
+/**
+ * This activity return the random number (1->n)
+ */
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
